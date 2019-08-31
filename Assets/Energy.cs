444b3f7a854061642ff34787cd8148b5ee24d8ea
joeface71 <joefacenda@yahoo.com>
2019-08-31 16:07:21 -0400

    process right click

--- Assets/Energy.cs
+++ Assets/Energy.cs
@@ -2,27 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using RPG.CameraUI;
 
 namespace RPG.Characters
 {
     public class Energy : MonoBehaviour
     {
-        [SerializeField] RawImage energyBar;
+        [SerializeField] RawImage energyBar = null;
         [SerializeField] float maxEnergyPoints = 100f;
+        [SerializeField] float pointsPerHit = 10f;
 
         float currentEnergyPoints;
+        CameraRaycaster cameraRaycaster;
+
 
         // Start is called before the first frame update
         void Start()
         {
             currentEnergyPoints = maxEnergyPoints;
+            cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+            cameraRaycaster.notifyRightClickObservers += ProcessRightClick;
         }
 
-        // Update is called once per frame
-        void Update()
+        void ProcessRightClick(RaycastHit raycastHit, int layerHit)
         {
+            float newEnergyPoints = currentEnergyPoints - pointsPerHit;
+            currentEnergyPoints = Mathf.Clamp(newEnergyPoints, 0, maxEnergyPoints);
+            
+            UpdateEnergyBar();
+        }
 
+        private void UpdateEnergyBar()
+        {
+            float xValue = -(EnergyAsPercent() / 2f) - 0.5f;
+            energyBar.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+        }
+
+        float EnergyAsPercent()
+        {
+            return currentEnergyPoints / maxEnergyPoints;
         }
     }
-
 }
+
+
